422cc36a5d6aec942530aef895d4fd1b5a851d92
Talha Naeem <50256402+Talha-Naeem@users.noreply.github.com>
2025-09-27 18:14:39 +0500

    frontend_monitoring.py

--- src/frontend_monitoring.py
+++ src/frontend_monitoring.py
@@ -1,7 +1,6 @@
 # src/frontend_monitoring.py
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
